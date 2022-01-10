dcb329a962b95ee8881898e06d1830f071d63780
JackOrt <jackjortega@gmail.com>
2022-01-09 17:23:42 -0800

    Form.js added checkpoint

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,17 +1,9 @@
 import React, {useState} from 'react';
 import Table from './Table';
+import Form from './Form';
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-         // the rest of the data
-      },{
-        name: 'Jack',
-        job: 'Test'    
-      }
-    ]);  
+    const [characters, setCharacters] = useState([]);  
 
 function removeOneCharacter (index) {
   const updated = characters.filter((character, i) => {
@@ -22,6 +14,7 @@ function removeOneCharacter (index) {
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form />
     </div>
   )
 
