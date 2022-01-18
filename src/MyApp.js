562934db5ea3a0920b6a7ba258c0400c3ffcceb6
JackOrt <jackjortega@gmail.com>
2022-01-18 01:34:48 -0800

    WORK IN PROGRESS COMMIT

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,14 +1,29 @@
-import React, {useState} from 'react';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 import Table from './Table';
 import Form from './Form';
 
 function MyApp() {
     const [characters, setCharacters] = useState([]);  
 
+    useEffect(() => {
+      fetchAll().then( result => {
+        console.log(result.status); 
+        if (result)
+            setCharacters(result);
+       });
+   }, [] );
+
 function removeOneCharacter (index) {
   const updated = characters.filter((character, i) => {
       return i !== index
     });
+    console.log("updated id: " + characters[index].id);
+    deleteByID(characters[index].id).then( result => {
+      //console.log(result.status); 
+      //if (result)
+          //setCharacters(result);
+     });
     setCharacters(updated);
   }
   return (
@@ -19,7 +34,79 @@ function removeOneCharacter (index) {
   )
 
   function updateList(person) {
-    setCharacters([...characters, person]);
+    post(person.name, person.job).then( result => {
+      if (result) {
+        setCharacters([...characters, person]);
+      }
+
+    });    
+    
+  }
+  
+}
+
+
+async function post(newName, newJob){
+  try {
+    
+     const response = await axios.post('http://localhost:5000/users', {
+      name: newName,
+      job: newJob,
+      id: "",
+    });
+     if (response.status == 200) {
+       console.log("posted status: " + response.status);
+       return response;
+     } else 
+     {
+      console.log("not posted status: " + response.status);
+      //return response;
+     }     
+  }
+  catch (error){
+     //We're not handling errors. Just logging into the console.
+     console.log(error); 
+     return false;         
+  }
+}
+
+
+async function deleteByID(id){
+  try {
+     const response = await axios.delete('http://localhost:5000/users' + '/' + id);
+     if (response.status == 200) {
+       console.log("deleted status: " + response.status);
+       //return response.data.users_list;
+     } else 
+     {
+      console.log("not deleted status: " + response.status);
+      //return response.data.users_list;
+     }     
+  }
+  catch (error){
+     //We're not handling errors. Just logging into the console.
+     console.log(error); 
+     return false;         
+  }
+}
+
+
+async function fetchAll(){
+  try {
+     const response = await axios.get('http://localhost:5000/users');
+     if (response.status == 201) {
+       console.log(response.status);
+       return response.data.users_list;
+     } else 
+     {
+      console.log(response.status);
+      return response.data.users_list;
+     }     
+  }
+  catch (error){
+     //We're not handling errors. Just logging into the console.
+     console.log(error); 
+     return false;         
   }
 }
 
